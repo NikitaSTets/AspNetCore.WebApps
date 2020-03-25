d0d0bfb81842e3e62b6661957e0dc5590b7af0b8
Nikita <nikita.stets.999@mail.ru>
2020-03-25 23:24:11 +0300

    tag helper was added

---
+++ AspNetCore.WebApps/TagHelpers/ExampleTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCore.WebApps.TagHelpers
+{
+    [HtmlTargetElement("my-first-tag-helper")]
+    public class ExampleTagHelper : TagHelper
+    {
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ExampleTagHelper1";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<span>Hi! {0}</span>", this.Name);
+
+            output.PreContent.SetHtmlContent(sb.ToString());
+        }
+    }
+}
